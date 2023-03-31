06004467f1747b2129e3e0847cd06723455a69b7
gilsonmuniz <gilsonj725@gmail.com>
2023-03-30 21:43:25 -0300

    Adding cpp codes

---
+++ read_xml.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include "tinyxml2.h"
+
+using namespace tinyxml2;
+
+int main() {
+  XMLDocument doc;
+  doc.LoadFile("example.xml");
+
+  // check for errors while loading the file
+  if (doc.ErrorID() != 0) {
+    std::cout << "Error loading file: " << doc.ErrorStr() << std::endl;
+    return 1;
+  }
+
+  // get the root element
+  XMLElement* root = doc.RootElement();
+
+  // loop through all child elements
+  for (XMLElement* elem = root->FirstChildElement(); elem != NULL; elem = elem->NextSiblingElement()) {
+    // get the element name and value
+    const char* name = elem->Name();
+    const char* value = elem->GetText();
+
+    // print the element name and value
+    std::cout << name << ": " << value << std::endl;
+  }
+
+  return 0;
+}

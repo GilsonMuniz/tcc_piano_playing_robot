df02910a4639e40a411e16e83299c61c2e75650f
GilsonMuniz <gilsonj725@gmail.com>
2023-05-28 19:54:32 -0300

    Saving music data in xlsx file

--- read_xml.py
+++ read_xml.py
@@ -1,4 +1,5 @@
 import xml.etree.ElementTree as ET
+from openpyxl import Workbook
 
 def parse_xml(filename):
     tree = ET.parse(filename)
@@ -26,4 +27,20 @@ def extract_notes(element, music):
 # Usage example
 xml_file = 'titanic.xml'
 music_list = parse_xml(xml_file)
-print(music_list)
+
+# Create an Excel workbook and select the active sheet
+workbook = Workbook()
+sheet = workbook.active
+
+# Write the header row
+header_row = ['Step', 'Octave', 'Duration']
+sheet.append(header_row)
+
+# Write the music data rows
+for note in music_list:
+    row = [note['step'], note['octave'], note['duration']]
+    sheet.append(row)
+
+# Save the workbook
+output_file = 'titanic.xlsx'
+workbook.save(output_file)

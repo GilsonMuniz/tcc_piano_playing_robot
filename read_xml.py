13e6067fbddf02e53395998b927dadf44929b705
gilsonmuniz <gilsonj725@gmail.com>
2023-06-08 18:31:44 -0300

    Saving excel with alters

--- read_xml.py
+++ read_xml.py
@@ -11,25 +11,29 @@ def parse_xml(filename):
 def extract_notes(element, music):
     for note in element.iter('note'):
         step_element = note.find('.//step')
+        alter_element = note.find('.//alter')
         octave_element = note.find('.//octave')
         duration_element = note.find('.//duration')
         rest_element = note.find('.//rest')
 
         if step_element is not None and octave_element is not None and duration_element is not None:
             step = step_element.text
-            octave = octave_element.text
-            duration = duration_element.text
+            octave = int(octave_element.text)
+            duration = int(duration_element.text)
+            alter = int(alter_element.text)
+            if alter == 1: step += '#'
+            if alter == -1: step += 'b'
             note_data = {'step': step, 'octave': octave, 'duration': duration}
             music.append(note_data)
         elif rest_element is not None and duration_element is not None:
             step = 'R' # rest
             octave = '-'
-            duration = duration_element.text
+            duration = int(duration_element.text)
             note_data = {'step': step, 'octave': octave, 'duration': duration}
             music.append(note_data)
 
 # Usage example
-xml_file = 'titanic.xml'
+xml_file = 'tristeza.xml'
 music_list = parse_xml(xml_file)
 
 # Create an Excel workbook and select the active sheet
@@ -46,5 +50,5 @@ for note in music_list:
     sheet.append(row)
 
 # Save the workbook
-output_file = 'titanic.xlsx'
+output_file = 'tristeza.xlsx'
 workbook.save(f'excel_scores/{output_file}')
